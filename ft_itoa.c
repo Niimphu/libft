a4331a62ec0a0c51b8309d85c2a6685030773475
yiwong <yiwong@student.42wolfsburg.de>
2023-01-18 16:25:25 +0100

    itoaFinal

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,55 +6,25 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 18:32:13 by yiwong            #+#    #+#             */
-/*   Updated: 2023/01/18 13:06:51 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/01/18 16:23:10 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa_rev(char *r, int sign, int j)
+char	*ft_itoa_gen(char *r, int n, int len, int sign)
 {
-	int		i;
-	char	x;
-
-	i = 0;
-	if (sign == -1)
-	{
-		r[i] = '-';
-		i++;
-	}
-	j--;
-	while (i < j)
-	{
-		x = r[i];
-		r[i] = r[j];
-		r[j] = x;
-		i++;
-		j--;
-	}
-	return (r);
-}
-
-char	*ft_itoa_gen(char *r, int n)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = ft_itoa_len(n);
-	x = 1;
-	while (len--)
-		x *= 10;
 	while (n)
 	{
-		r[i] = (n % 10) - '0';
+		r[--len] = (n % 10) + '0';
 		n /= 10;
-		i++;
 	}
+	if (sign == -1)
+		r[0] = '-';
 	return (r);
 }
 
-int	ft_itoa_len(n)
+int	ft_itoa_len(int n)
 {
 	int	count;
 
@@ -74,14 +44,14 @@ int	ft_itoa_len(n)
 
 char	*ft_itoa(int n)
 {
-	int		i;
 	int		sign;
 	int		len;
 	char	*r;
-	char	*t;
 
-	if n = (-2147483648)
+	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
+	if (n == 0)
+		return (ft_strdup("0"));
 	len = ft_itoa_len(n);
 	r = malloc(sizeof(char) * (len + 1));
 	if (!r)
@@ -90,8 +60,7 @@ char	*ft_itoa(int n)
 	if (n < 0)
 		sign = -1;
 	n *= sign;
-	ft_itoa_gen(r, n);
-	ft_itoa_rev(r, sign, len);
+	ft_itoa_gen(r, n, len, sign);
 	r[len] = '\0';
 	return (r);
 }
