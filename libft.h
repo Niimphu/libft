c90882ca8d78e42c2eaa0f973dc55b8ed8496216
yiwong <yiwong@student.42wolfsburg.de>
2023-01-24 21:21:53 +0100

    lstaddback

--- libft.h
+++ libft.h
@@ -65,7 +65,8 @@ void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
-t_list	*ft_lstlast(t_list *lst);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
